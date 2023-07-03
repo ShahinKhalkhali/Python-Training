2576199a86894f78f841e705406d44030050c777
Shahin Khalkhali <shahin.khalkhali94@hotmail.com>
2023-07-03 18:45:45 -0400

    Update

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 ###############      PYTHON TRAINING      ###############
 ###############   SHAHIN K. & SHAYAN K.   ###############
 #########################################################
+
 # Example 1 (Printing)
 print ("Day 1 - Python Print Function\nThe fucntion is declared like this\nprint('what to print')\n") # New line
 print ("Hello" + " " + "World") # Concatenation
+input ("What's your name: ") # print using input
 
 # Example 2
