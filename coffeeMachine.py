93bf43c45dcc054fc6e8ff86f66b580f563b34ad
Shahin Khalkhali <shahin.khalkhali94@hotmail.com>
2023-07-04 02:00:44 -0400

    Added Comments to Coffee Machine Project

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -3,13 +3,29 @@
 ###############         PROEJECT          ###############
 #########################################################
 
+# Espresso      --> Requires:   50ml Water 
+#                               18g Coffee
+
+# Latte         --> Requires:   200ml Water 
+#                               24g Coffee
+#                               150ml Milk
+
+# Cappuccino    --> Requires:   250ml Water 
+#                               24g Coffee
+#                               100ml Milk
+
 coffeeType = input ("What would you like? (Espresso/Latte/Cappuccino): ")
+
+#TODO ADD LOOP CONDITION UNTIL WATER/MILK/COFFEE/MONEY MEETS CONDITIONS
 
 if coffeeType == "Latte":
     print("Distributing latte")
+    #TODO REMOVE RESOURCE FROM MACHINE
 
 if coffeeType == "Espresso":
     print("Distributing espresso")
+    #TODO REMOVE RESOURCE FROM MACHINE
 
 if coffeeType == "Cappuccino":
     print("Distributing cappuccino")
+    #TODO REMOVE RESOURCE FROM MACHINE
