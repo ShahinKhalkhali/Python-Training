4b68f5c15804700a85f8d80b319726c8c3bcd655
Shahin Khalkhali <shahin.khalkhali94@hotmail.com>
2023-07-04 14:47:06 -0400

    Updated Coffee Machine

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -22,28 +22,40 @@ capacityWater = 1000
 capacityCoffee = 1000
 capacityMilk = 1000
 capacityMoney = 0
-
-# Input
-coffeeType = input ("What would you like? (Espresso/Latte/Cappuccino): ")
-capacityMoney = float(input("Insert Money: "))
-
-#TODO ADD LOOP CONDITION WHILE WATER/MILK/COFFEE/MONEY MEETS CONDITIONS
+capacityChange = 0
 
 while capacityWater > 0 and capacityCoffee > 0 and capacityMilk > 0:
+    # Input
+    coffeeType = input ("What would you like? (Espresso/Latte/Cappuccino): ")
+    capacityMoney = float(input("Insert Money: "))
 
     if capacityMoney >= 1.5:
         if coffeeType == "Latte":
-            print("Distributing latte")
-            #TODO REMOVE RESOURCE FROM MACHINE FUNCTION
-
+            capacityWater -= 50
+            capacityCoffee -= 18
+            capacityChange = capacityMoney - 1.5
+            print("Distributing latte\n")
 
     if capacityMoney >= 2.5:
         if coffeeType == "Espresso":
-            print("Distributing espresso")
-            #TODO REMOVE RESOURCE FROM MACHINE FUNCTION
-
+            capacityWater -= 200
+            capacityCoffee -= 24
+            capacityMilk -= 150
+            capacityChange = capacityMoney - 2.5
+            print("Distributing espresso\n")
 
     if capacityMoney >= 3.0:
         if coffeeType == "Cappuccino":
-            print("Distributing cappuccino")
-            #TODO REMOVE RESOURCE FROM MACHINE FUNCTION
+            capacityWater -= 250
+            capacityCoffee -= 24
+            capacityMilk -= 100
+            capacityChange = capacityMoney - 3.0
+            print("Distributing cappuccino\n")
+
+    # Feedback
+    print ("Money returned: " + str(capacityChange) + "$\n")
+
+    print ("Resource Capacity")
+    print ("Water left: " + str(capacityWater) + "ml")
+    print ("Coffee left: " + str(capacityCoffee) + "g")
+    print ("Milk left: " + str(capacityMilk) + "ml\n")
