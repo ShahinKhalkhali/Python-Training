14d59368a27db261b8048fc03983cc0a06c7bb83
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-05-17 17:02:33 -0400

    Band name generator

---
+++ bandNameGenerator.py
@@ -0,0 +1,9 @@
+##############################################################
+###############      BAND NAME GENERATOR       ###############
+##############################################################
+
+print("This is a band name generator\n")
+
+city_name = input("What is the name of your city: ")
+pet_name = input("\nWhat is your first pet's name: ")
+band_name = print("\nYour band name could be: " + city_name + " " + pet_name)
