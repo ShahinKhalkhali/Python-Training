adf00830b296c90242018bf61c4408fa6bf17093
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-05-19 17:06:22 -0400

    Update rock paper scissor project

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -4,76 +4,64 @@
 
 import random
 
-choice_rock = 0
-choice_paper = 1
-choice_scissor = 2
+choice_rock ='''
+        _______
+    ---'   ____)
+           (_____)
+           (_____)   ROCK
+           (____)
+    ---.__(___)
+'''
+
+choice_paper ='''
+         _______
+    ---'    ____)____
+               ______)
+              _______)    PAPER
+             _______)
+    ---.__________)
+'''
+
+choice_scissor = '''
+        _______
+    ---'   ____)____
+              ______)
+           __________)    SCISSOR
+          (____)
+    ---.__(___)
+'''
+
+game_images = [choice_rock, choice_paper, choice_scissor]
 
 player_choice = int(input("Welcome to the Rock Paper Scissors Game, What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
 CPU_choice = random.randint(0, 2)
 
+# Catch error
+while player_choice < 0 or player_choice > 2:
+    if player_choice < 0 or player_choice > 2:
+        player_choice = int(input("\nYou have chosen an invalid number, please type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
+
 print("Your choice:")
 
-if player_choice == choice_rock:
-    print("""
-        _______
-    ---'   ____)
-           (_____)
-           (_____)   ROCK
-           (____)
-    ---.__(___)
-    """)
+if player_choice == 0:
+    print(game_images[0])
 
-elif player_choice == choice_paper:
-    print("""
-         _______
-    ---'    ____)____
-               ______)
-              _______)    PAPER
-             _______)
-    ---.__________)
-    """)
+elif player_choice == 1:
+    print(game_images[1])
 
-elif player_choice == choice_scissor:
-    print("""
-        _______
-    ---'   ____)____
-              ______)
-           __________)    SCISSOR
-          (____)
-    ---.__(___)
-    """)
+elif player_choice == 2:
+    print(game_images[2])
 
 print("CPU Choice:\n")
 
-if CPU_choice == choice_rock:
-    print("""
-        _______
-    ---'   ____)
-           (_____)
-           (_____)   ROCK
-           (____)
-    ---.__(___)
-    """)
+if CPU_choice == 0:
+    print(game_images[0])
 
-elif CPU_choice == choice_paper:
-    print("""
-         _______
-    ---'    ____)____
-               ______)
-              _______)    PAPER
-             _______)
-    ---.__________)
-    """)
+elif CPU_choice == 1:
+    print(game_images[1])
 
-elif CPU_choice == choice_scissor:
-    print("""
-        _______
-    ---'   ____)____
-              ______)
-           __________)    SCISSOR
-          (____)
-    ---.__(___)
-    """)
+elif CPU_choice == 2:
+    print(game_images[2])
 
 if player_choice == 0 and CPU_choice == 1:
     print("\nYOU LOST!")
