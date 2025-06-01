0498049e6a5c592a44e0dcc25e267df2d88f2875
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-06-01 16:18:04 -0400

    Update comments

--- Hangman/hangman_main.py
+++ Hangman/hangman_main.py
@@ -2,19 +2,18 @@
 ###############      HANGMAN        ###############
 ###################################################
 
-import random
-import hangman_words as WL
-import hangman_art as T
+import random # Imports entire random module
+import hangman_art as T # Imports entire hangman_art module with alias T to call particular function
+from hangman_words import word_list # Imports only the list word_list
 
 print("Welcome to Hangman")
-
-from hangman_words import word_list
 
 word_letters = []
 guess_display = []
 wrong_guesses = 0
 flag = False
 
+# 
 def display(word_length, guess_display, chosen_word):
 
     count = len(chosen_word)
@@ -40,27 +39,29 @@ for i in word_length:
     word_letters.append(chosen_word[i])
     guess_display.append(guess_letters)
 
+# Uncomment following prints to see which word was picked
 # print(f"word_chosen: {chosen_word}")
 # print(f"word_letters: {word_letters}")
-# print(f"guess_display: {guess_display}")
 
 display(word_length, guess_display, chosen_word)
 
+# Iterate over choices
 while wrong_guesses <= 6:
     user_choice = input("\nChoose guess: ")
     
+    # Check each letter from randomly chosen word and flag if a letter was picked
     for i in word_length:
-        # print(f"Letter {i}: {word_letters[i]}")
         if user_choice == word_letters[i].lower():
             flag = True
             guess_display[i] = word_letters[i]
-            # print(f"guess_display: {guess_display}")
             display(word_length, guess_display, chosen_word)
 
+    # If flag is still false it means user made chose a wrong letter
     if flag == False:
         wrong_guesses = wrong_guesses + 1
         T.hangman(word_length, guess_display, wrong_guesses)
 
+    # If we reach total number of wrong guesses, end game
     if wrong_guesses == 6:
         T.hangman(word_length, guess_display, wrong_guesses)
         print(f"\nGame Over.")
