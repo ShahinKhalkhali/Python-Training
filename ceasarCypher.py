f65873600ed6c0a7c3248563a83962ad240c8290
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-06-14 19:02:54 -0400

    Update

--- ceasarCypher.py
+++ ceasarCypher.py
@@ -1,9 +1,14 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'n', 'o', 'p', 'q', 'r', 
             's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
 text = input("Type your message: ").lower()
 shift = int(input("Type the shift number: "))
+
+# TODO-1: Create function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs
+# TODO-2: In encrypt() shift each letter forwards by a shift amount and print
+# TODO-3: Call encrypt() and pass in the user inputs. You should be able to test the code and encrypt message
+# TODO-4: What happens if you try to shift z by 9? Can you shift the code?
 
 # Primitive encryption function (Longer)
 def encrypt_prim(original_text, shift_amount):
@@ -40,13 +45,33 @@ def encrypt_adv(original_text, shift_amount):
 
         modified_text += alphabet[shifted_position]         # Constructs string
     
-    print(f"The Encrypted message is: {modified_text}")
+    print(f"The encrypted message is: {modified_text}")
 
-# TODO - Decryption
-def decrypt(encrypted_text):
-    return 0
+# TODO-1: Create a function called decrypt() that takes original_text and shift_amount as 2 inputs
+# TODO-2: Inside the decrypt() function, shift each letter of the original_text forwards in the alphabet backwards by the shift_amount and print the decrypted text
+# TODO-3:   1) Combine the encrypt() and decrypt() functions into a single function called caesar()
+#           2) Use the value of the user chosen direction variable to determine which functionality to use
+#           3) Call the caesar function instead of encrypt/decrypt and pass in all three variables (ie: direction/text/shift)
 
+def decrypt(original_text, shift_amount):
+    modified_text = ""
+
+    for i in original_text:
+        shifted_position = alphabet.index(i) - shift_amount
+        shifted_position = shifted_position % len(alphabet)
+        modified_text += alphabet[shifted_position]
+
+    print(f"The decrypted message is: {modified_text}")
+
+def caesar(direction, original_text, shift_amount):
+    if direction.lower() == "encrypt":
+        encrypt_adv(original_text, shift_amount)
+    elif direction.lower() == "decrypt":
+        decrypt(original_text, shift_amount)
+    else:
+        print("Please choose to either 'Encrypt' or 'Decrypt'.")
+        caesar(direction, original_text, shift_amount)
 
 chars_text = list(text) # Transforming text into a list of characters
-encrypt_prim(chars_text, shift) # My method
-encrypt_adv(text, shift) # Angela's method
+
+caesar(direction, text, shift)
