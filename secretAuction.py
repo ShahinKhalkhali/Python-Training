bcf16b7faa9a517547dab6121220c4977cf31341
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-06-29 00:49:27 -0400

    Update

--- secretAuction.py
+++ secretAuction.py
@@ -2,30 +2,40 @@
 ###############      SECRET AUCTION        ##############
 #########################################################
 
-bidderDict = {}
-flag = True
+import os
+
+auctionDict = {}
+nameDict = []
+bidDict = []
 
 # def sortBids():
 
-def main(bidderDict, name, bid):
+name = input("What is your name?: ")
+bid = int(input("What's your bid?: $"))
+
+auctionDict[name] = bid
+
+def main(auctionDict):
+    flag = True
+    i = 0
     while flag == True:
         i += i
         flag = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+
         if flag == "yes":
+            os.system('cls')
             flag = True
+            name = input("What is your name?: ")
+            bid = int(input("What's your bid?: $"))
+            auctionDict[name] = bid
+
         elif flag == "no":
-            exit # Replace this with sorting algorithm to display winner of bid
+            print(f"Names & bids: {auctionDict}")
+            #TODO: Write sorting algorith sortBids()
+            exit
+
         else:
             flag = input("Please input either 'yes' or 'no'\n")
             main()
 
-        name = input("What is your name?: ")
-        bid = int(input("What's your bid?: "))
-
-        bidderDict[name][bid]
-
-name = str(input("What is your name?: "))
-bid = int(input("What's your bid?: $"))
-bidderDict[name][bid]
-
-main(bidderDict, name, bid)
+main(auctionDict)
