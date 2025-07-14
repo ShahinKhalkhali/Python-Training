005dbfe89694d674e4d6a8f0c8d24363d20d88de
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-07-14 01:24:44 -0400

    Update

--- 011. Day 11 - Blackjack Capstone Project/blackjackProject.py
+++ 011. Day 11 - Blackjack Capstone Project/blackjackProject.py
@@ -13,25 +13,30 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 player_hand = [random.choice(cards), random.choice(cards)]
 computer_hand = [random.choice(cards)]
-player_hand_sum = player_hand[0] + player_hand[1]
+player_hand_sum = sum(player_hand)
 
-flag = True
-
-def hit_stand_function():
+def hit_stand_function(player_hand, player_hand_sum):
 
     hit_stand = input("Type 'H' to Hit or 'S' to Stand: ")
 
     if hit_stand.lower() == 'h':
-        # TODO: Insert function for hit
         print("Hit!")
-        player_hand.insert(1, random.choice(cards))
-        if player_hand_sum > 21:
+        player_hand.insert(0, random.choice(cards))
+        player_hand_sum = sum(player_hand)
+
+        if player_hand_sum > 21:    # TODO: Add condition that if the number if '11' replace it with '1'
+            print(f"Your cards: {player_hand}, current score: {player_hand_sum}")
             print(f"Bust, your score is {player_hand_sum}! Better luck next time :(")
             play_again()
+            
         elif player_hand_sum < 21:
-            print(f"Your score is {player_hand_sum}")
-            return player_hand
-
+            print(f"Your cards: {player_hand}, current score: {player_hand_sum}")
+            print(f"Computer's first card: {computer_hand}")
+            return player_hand_sum
+        
+        else:
+            return player_hand_sum
+        
     elif hit_stand.lower() == 's':
         # TODO: Insert function for stand
         print("Stand!")
@@ -51,13 +56,16 @@ def play_again():
 
 def main():
 
+    flag = True
+
     while flag == True:
-        print(f"Your cards: {player_hand}, current score: {player_hand_sum}")
-        print(f"Computer's first card: {computer_hand}")
         
         if player_hand_sum == 21:
             print(f"You got Black Jack!")
             # TODO: Insert function for computer's play
 
-        hit_stand_function()
-        player_hand_sum = sum()
+        hit_stand_function(player_hand, player_hand_sum)
+
+print(f"Your cards: {player_hand}, current score: {player_hand_sum}")
+print(f"Computer's first card: {computer_hand}")
+main()
