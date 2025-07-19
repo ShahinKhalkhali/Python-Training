d33b04cbbdada4e7d5a6fc8f1e4c734363fdb620
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-07-19 13:30:33 -0400

    Day 11 - Complete

--- 011. Day 11 - Blackjack Capstone Project/blackjackProject.py
+++ 011. Day 11 - Blackjack Capstone Project/blackjackProject.py
@@ -17,7 +17,7 @@ computer_hand_sum = sum(computer_hand)
 
 def hit_stand_function(player_hand, player_hand_sum, computer_hand, computer_hand_sum):
 
-    """Determines the player's choice.
+    """Handles the player's decision to Hit or Stand and updates game state accordingly.
 
     Args:
         player_hand (list): List of player cards.
@@ -64,6 +64,21 @@ def hit_stand_function(player_hand, player_hand_sum, computer_hand, computer_han
         hit_stand_function(player_hand, player_hand_sum, computer_hand, computer_hand_sum)
 
 def computer_function(player_hand, computer_hand, player_hand_sum, computer_hand_sum):
+
+    """Handles the dealer's decision to Hit or Stand.
+
+    Args:
+        player_hand (list): List of player cards.
+        computer_hand (list): List of  dealer cards.
+        player_hand_sum (int): Total value of player's hand.
+        computer_hand_sum (int): Total value in dealer's hand.
+
+    Returns:
+        tuple: A tuple containing:
+        - list: Updated computer hand.
+        - int: Updated total sum of computer hand.
+    """
+
     print("\n-------------- Computer's Turn --------------\n")
     flag = True
     while flag == True:
@@ -84,6 +99,17 @@ def computer_function(player_hand, computer_hand, player_hand_sum, computer_hand
             computer_hand_sum = sum(computer_hand)
 
 def play_again(player_hand_sum, computer_hand_sum):
+
+    """Handles the player's decision to play again or not.
+
+    Args:
+        player_hand_sum: int
+        computer_hand_sum: int
+
+    Returns:
+        None.
+    """
+
     win_condition(player_hand_sum, computer_hand_sum)
     while True:
         replay = input("\nDo you want to play another game of Blackjack? Type 'Y' or 'N': ")
@@ -99,10 +125,34 @@ def play_again(player_hand_sum, computer_hand_sum):
             print("\nPlease enter a valid input.")
 
 def print_score(player_hand, computer_hand, player_hand_sum, computer_hand_sum):
+
+    """Displays the player's and dealer's hand & total score.
+
+    Args:
+        player_hand (list): List of player cards.
+        computer_hand (list): List of  dealer cards.
+        player_hand_sum (int): Total value of player's hand.
+        computer_hand_sum (int): Total value in dealer's hand.
+    
+    Returns:
+        None.
+    """
+
     print(f"\nYour cards: {player_hand}, current score: {player_hand_sum}")
     print(f"Computer's cards: {computer_hand}, computer's score: {computer_hand_sum}")
 
 def win_condition(player_hand_sum, computer_hand_sum):
+
+    """Displays the winner of the game.
+    
+    Args:
+        player_hand_sum: int
+        computer_hand_sum: int
+
+    Returns:
+        None.
+    """
+
     if player_hand_sum < 22 and computer_hand_sum < 22 and computer_hand_sum > player_hand_sum:
         print(f"\nComputer Wins with {computer_hand_sum}! Better luck next time... :(")
     elif player_hand_sum < 22 and computer_hand_sum < 22 and computer_hand_sum < player_hand_sum:
@@ -115,11 +165,23 @@ def win_condition(player_hand_sum, computer_hand_sum):
         print(f"\nDealer bust with {computer_hand_sum}, Congrats you win with {player_hand_sum}! :D")
 
 def main(player_hand, computer_hand, player_hand_sum, computer_hand_sum):
+
+    """Main function that begins the game.
+
+    Args:
+        player_hand (list): List of player cards.
+        computer_hand (list): List of  dealer cards.
+        player_hand_sum (int): Total value of player's hand.
+        computer_hand_sum (int): Total value in dealer's hand.
+    
+    Returns:
+        None.
+    """
+
     print("\n-------------- Player's Turn --------------\n")
     print(f"Your cards: {player_hand}, current score: {player_hand_sum}")
     print(f"Computer's first card: {computer_hand}")
-    flag = True
-    while flag == True:
+    while True:
         if player_hand_sum == 21:
             print(f"\nYou got Black Jack!")
             computer_function(player_hand, computer_hand, player_hand_sum, computer_hand_sum)
