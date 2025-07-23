39414f8983c8f78d37aa20ae0508c4873739bae1
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-07-23 02:25:34 -0400

    Day 12 - Complete

--- 012. Day 12 - Scope & Number Guessing/numberGuessingProject.py
+++ 012. Day 12 - Scope & Number Guessing/numberGuessingProject.py
@@ -6,34 +6,58 @@ print('''
 
 import random
 
-randNumber = random.randint(0,100)
+randNumber = int(random.randint(0,100))
 
-def compareGuess(numberChosen, randNumber):
+def compare_guess(randNumber, difficulty, flag):
+    numberChosen = int(input("Choose a number to guess: "))
     print(f"numberChosen: {numberChosen}\nrandNumber: {randNumber}")
-    if difficulty == "hard":
+    if difficulty.lower() == "hard":
         while flag < 6:
             if numberChosen > randNumber:
                 print(f"Number is too high")
                 flag += 1
+                compare_guess(randNumber, difficulty, flag)
             elif numberChosen < randNumber:
                 print(f"Number is too low")
                 flag += 1
+                compare_guess(randNumber, difficulty, flag)
             elif numberChosen == randNumber:
                 print(f"You guessed the right number!")
-                
-    elif difficulty == "easy":
+                play_again()
+
+        print("You have no more guesses.\n")
+        play_again()
+
+    elif difficulty.lower() == "easy":
         while flag < 11:
             if numberChosen > randNumber:
                 print(f"Number is too high")
                 flag += 1
+                compare_guess(randNumber, difficulty, flag)
             elif numberChosen < randNumber:
                 print(f"Number is too low")
                 flag += 1
+                compare_guess(randNumber, difficulty, flag)
             elif numberChosen == randNumber:
                 print(f"You guessed the right number!")
+                play_again()
+
+        print("\nYou ran out of guesses! :()")
+        play_again()
+
+def play_again():
+    while True:
+        replay = input("\nDo you want to play another game of Blackjack? Type 'Y' or 'N': ")
+        if replay.lower() == 'y':
+            main()
+        elif replay.lower() == 'n':
+            exit()
+        else:
+            print("\nPlease enter a valid input.")
 
 def main():
-    numberChosen = input("Choose a number to guess: ")
-    compareGuess(numberChosen, randNumber)
+    flag = 0
+    difficulty = input("Choose a difficulty (Hard or Easy): ")
+    compare_guess(randNumber, difficulty, flag)
 
 main()
